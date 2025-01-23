0f3073889bd2506c843eb66c79f87c90c902cdb5
Alpha1337k <oscar@kruithof.io>
2025-01-23 17:01:26 +0100

    Feat: seems to be working!

--- src/csp.py
+++ src/csp.py
@@ -7,14 +7,14 @@ from train import load_data
 
 def test_real():
 	csp = CSP(n_components=4)
-	epochs = load_data([1], [3])
+	epochs = load_data([1], [i for i in range(1,14)])
 
 	X = epochs.get_data(copy=False)
 	Y = epochs.events[:, -1] - 1
 
 	print(Y[0:1])
 
-	csp.fit(X[0:2], Y[0:2])
+	csp.fit(X, Y)
 
 	X1Res = csp.transform(X[0:1])
 	X2Res = csp.transform(X[1:2])
@@ -25,30 +25,29 @@ def test_real():
 	plt.clf()
 
 def run_csp():
-	# test_real()
-# 
-	# exit(1)
+# 	test_real()
+# # 
+# 	exit(1)
 
-	epochs = load_data([1], [3])
+	epochs = load_data([1], [i for i in range(1,15)])
 
-	components = 4
+	components = 8
 
 	X = epochs.get_data(copy=False)
 	Y = epochs.events[:, -1] - 1
 
 	print(X.shape)
 
-	X = X.mean(axis=1)
-
-	print(X.shape)
+	# X = X.mean(axis=1)
+	# print(X.shape)
 
 	print(Y)
 
-	X1: np.ndarray = X[np.where(Y[:-1] == 0)]
-	X2: np.ndarray = X[np.where(Y[:-1] == 1)]
+	X1: np.ndarray = X[np.where(Y == 0)]
+	X2: np.ndarray = X[np.where(Y == 1)]
 
-	plt.scatter(X1[:, 0], X1[:, 1], c='red')
-	plt.scatter(X2[:, 0], X2[:, 1], c='blue')
+	plt.scatter(X1[:, 0], X1[:, 2], c='red')
+	plt.scatter(X2[:, 0], X2[:, 2], c='blue')
 	plt.savefig("results/csp/before.png")
 	plt.clf()
 
@@ -57,8 +56,10 @@ def run_csp():
 	# R1 = (X1 @ X1.transpose()) / len(X1)
 	# R2 = (X2 @ X2.transpose()) / len(X2)
 
-	R1 = np.cov(X1)
-	R2 = np.cov(X2)
+	print("TPS: ", X1.shape)
+
+	R1 = np.cov(X1.transpose(1,0,2).reshape(40, -1))
+	R2 = np.cov(X2.transpose(1,0,2).reshape(40, -1))
 	
 
 	print(R1.shape)
@@ -74,15 +75,22 @@ def run_csp():
 
 	filter = eigen_vectors[ : components]
 
-	X1Res = (filter @ X1).transpose()
-	X2Res = (filter @ X2).transpose()
+	print("Filter size: ", filter.shape)
+	print("X1 size: ", X1.shape)
+
+	XRes = np.asarray([filter @ x for x in X]).mean(axis=2)
+
+	XRes = XRes / np.linalg.norm(XRes)
+
+	X1Res: np.ndarray = XRes[np.where(Y == 0)]
+	X2Res: np.ndarray = XRes[np.where(Y == 1)]	
 
 	print(X1Res.shape)
 	print("----\n\n")
 	print(X2Res.shape)
 
 
-	plt.scatter(X1Res[:, 0], X1Res[:, 2], c='blue')
-	plt.scatter(X2Res[:, 0], X2Res[:, 2], c='red')
+	plt.scatter(X1Res[:, 0], X1Res[:, 5], c='blue')
+	plt.scatter(X2Res[:, 0], X2Res[:, 5], c='red')
 	plt.savefig("results/csp/after.png")
 	plt.clf()
