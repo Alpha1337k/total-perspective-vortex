82a834c67d2b46d05362c577c560f44c44ca82a7
Alpha1337k <oscar@kruithof.io>
2025-01-24 01:45:55 +0100

    Feat: moved to scikit supported class. Performance sub par

--- src/csp.py
+++ src/csp.py
@@ -1,9 +1,53 @@
 
 from matplotlib import pyplot as plt
-from mne.decoding import CSP
+# from mne.decoding import CSP
 import numpy as np
 from numpy import linalg
-from train import load_data
+from utils import load_data
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class CSP42(TransformerMixin, BaseEstimator):
+	filter = None
+	normalizer = None
+
+	def __init__(self, *, n_components=4):
+		self.n_components = n_components
+	def fit(self, X, y):
+		X1: np.ndarray = X[np.where(y == 0)]
+		X2: np.ndarray = X[np.where(y == 1)]
+
+		min_len = min(len(X1), len(X2))
+
+		X1 = X1[:min_len - 1]
+		X2 = X2[:min_len - 1]
+
+		R1 = np.cov(X1.transpose(1,0,2).reshape(40, -1))
+		R2 = np.cov(X2.transpose(1,0,2).reshape(40, -1))
+
+		combined = (R1 + R2) ** 2 @ R1
+
+		eigen_values, eigen_vectors = linalg.eigh(combined)
+
+		sorted_index = np.argsort(eigen_values)
+		eigen_vectors = eigen_vectors[:, sorted_index]
+
+		self.filter = eigen_vectors[ : self.n_components]
+		self.normalizer = np.linalg.norm(X1)
+
+		return self
+
+	def transform(self, X):		
+		XRes = np.asarray([self.filter @ x for x in X]).mean(axis=2)
+
+		# XRes = np.log(XRes)
+
+		# norm = np.linalg.norm(XRes)
+
+		# XRes /= norm
+		# np.nan_to_num(XRes, False, posinf=0.0, neginf=0.0)
+		
+		return XRes
+
 
 def test_real():
 	csp = CSP(n_components=4)
@@ -25,10 +69,6 @@ def test_real():
 	plt.clf()
 
 def run_csp():
-# 	test_real()
-# # 
-# 	exit(1)
-
 	epochs = load_data([1], [i for i in range(1,15)])
 
 	components = 8
@@ -53,44 +93,14 @@ def run_csp():
 
 	print("Divided: ", X1.shape, X2.shape)
 
-	# R1 = (X1 @ X1.transpose()) / len(X1)
-	# R2 = (X2 @ X2.transpose()) / len(X2)
+	csp = CSP42(n_components=8)
 
-	print("TPS: ", X1.shape)
-
-	R1 = np.cov(X1.transpose(1,0,2).reshape(40, -1))
-	R2 = np.cov(X2.transpose(1,0,2).reshape(40, -1))
-	
-
-	print(R1.shape)
-	print(R2.shape)
-
-	eigen_values, eigen_vectors = linalg.eig(R1)
-
-	sorted_index = np.argsort(eigen_values)
-
-	eigen_vectors = eigen_vectors[:, sorted_index]
-
-	print("----\n\n")
-
-	filter = eigen_vectors[ : components]
-
-	print("Filter size: ", filter.shape)
-	print("X1 size: ", X1.shape)
-
-	XRes = np.asarray([filter @ x for x in X]).mean(axis=2)
-
-	XRes = XRes / np.linalg.norm(XRes)
+	XRes = csp.fit_transform(X, Y)
 
 	X1Res: np.ndarray = XRes[np.where(Y == 0)]
-	X2Res: np.ndarray = XRes[np.where(Y == 1)]	
+	X2Res: np.ndarray = XRes[np.where(Y == 1)]
 
-	print(X1Res.shape)
-	print("----\n\n")
-	print(X2Res.shape)
-
-
-	plt.scatter(X1Res[:, 0], X1Res[:, 5], c='blue')
-	plt.scatter(X2Res[:, 0], X2Res[:, 5], c='red')
+	plt.scatter(X1Res[:, 1], X1Res[:, 4], c='blue')
+	plt.scatter(X2Res[:, 1], X2Res[:, 4], c='red')
 	plt.savefig("results/csp/after.png")
 	plt.clf()
--- src/train.py
+++ src/train.py
@@ -14,7 +14,9 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold, ShuffleSplit, cross_val_predict, cross_val_score, train_test_split
 from sklearn.pipeline import Pipeline
 
+from csp import CSP42
 from plot import make_exclude_list
+from utils import load_data
 
 experiments = [
 	[3, 7, 11],
@@ -26,64 +28,6 @@ experiments = [
 ]
 
 model_config = ConfigDict(arbitrary_types_allowed=True)
-
-@validate_call
-def load_data(sessions: List[int], runs: List[int]):
-	raws = []
-
-	for session in sessions:
-		# print(f"LOADING session #{session} ({len(raws)})")
-
-		new_raws = [
-			read_raw_edf(f"./data/S{session:03d}/S{session:03d}R{run:02d}.edf", preload=True) for run in runs
-		]
-
-
-		# print([r.info['sfreq'] for r in new_raws])
-
-		raws += new_raws
-
-	raw = concatenate_raws(raws)
-
-	raws = None
-
-	standardize(raw)  # set channel names
-	# montage = make_standard_montage("standard_1005")
-	montage = read_custom_montage("./data/custom_fixture.txt")
-
-	excluded_channels = make_exclude_list(montage.ch_names)
-
-	# print(excluded_channels)
-
-	raw.drop_channels(excluded_channels)
-
-	raw.set_montage(montage)
-
-	raw.annotations.rename(dict(T0="rest", T1="hands", T2="feet"))  # as documented on PhysioNet
-
-	raw.set_eeg_reference(projection=True)
-
-	raw.notch_filter(60, method="iir")
-	raw.filter(7.0, 32.0, 
-			fir_design="firwin", 
-			)
-
-	picks = pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude="bads")
-
-	epochs = Epochs(
-		raw,
-		proj=True,
-		event_id=["hands", "feet"],
-		picks=picks,
-		baseline=None,
-		# detrend=1,
-		# tmin=1,
-		# tmax=2,
-		preload=True,
-	)
-
-	return epochs
-
 
 @validate_call(config=model_config)
 def split_datasets(epochs: Epochs) -> Tuple[np.ndarray, np.ndarray, Any, np.ndarray, np.ndarray]:
@@ -102,19 +46,15 @@ def split_datasets(epochs: Epochs) -> Tuple[np.ndarray, np.ndarray, Any, np.ndar
 
 	return (Y_data, X, folder.split(X_data), X_test, Y_test)
 
-@validate_call(config=model_config)
-def fit(model: Pipeline, X: np.ndarray, Y: np.ndarray): 
-
-	model.fit(data, labels)
-
-	return model
-
 def train():
 	global total_correct, total_states
 	set_log_level(False)
 
-	for subject in range(1, 110):
-		csp = CSP(n_components=8, reg=None, log=True, norm_trace=False)
+	accuracies = []
+
+	for subject in range(1, 40):
+		# csp = CSP(n_components=8, reg=None, log=False, norm_trace=False)
+		csp = CSP42(n_components=8)
 		model = LinearDiscriminantAnalysis(solver='lsqr')
 
 		pipe = Pipeline([("CSP", csp), ("EST", model)])
@@ -122,7 +62,7 @@ def train():
 		epochs = load_data([subject], [i for i in range(3, 15)])
 
 		X = epochs.get_data(copy=False).astype(np.float64)
-		Y = epochs.events[:, -1] - 2
+		Y = epochs.events[:, -1] - 1
 
 		X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
@@ -133,7 +73,7 @@ def train():
 
 			epochs = load_data([subject], [i for i in range(3, 15)])
 			X = epochs.get_data(copy=False).astype(np.float64)
-			Y = epochs.events[:, -1] - 2
+			Y = epochs.events[:, -1] - 1
 
 			X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
@@ -141,8 +81,14 @@ def train():
 
 			print(f"Test Accuracy: {acc * 100:0.2f}%")
 
+			accuracies.append(acc)
+
 			scores = cross_val_score(pipe, X, Y, cv=KFold(n_splits=5, shuffle = True))
 			print(f"{100 * scores.mean():0.2f}% accuracy with a standard deviation of {100 * scores.std():0.2f}%" )
+		
+		
+		print(f"======== Total accuracy: {sum(accuracies) / len(accuracies) * 100:0.2f}%")
+
 
 
 
